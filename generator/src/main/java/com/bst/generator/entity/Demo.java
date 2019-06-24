c350d774bb9c7d4c84dc561334a2f598899cc0cb
zhajinyi <zhajinyi@163.com>
2019-06-24 16:03:56 +0800

    增加逻辑删除

--- generator/src/main/java/com/bst/generator/entity/Demo.java
+++ generator/src/main/java/com/bst/generator/entity/Demo.java
@@ -1,7 +1,14 @@
 package com.bst.generator.entity;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.bst.generator.base.BaseEntity;
 import com.bst.generator.dto.DemoDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Demo extends BaseEntity<DemoDto> {
+    @TableLogic
+    @JsonIgnore
+    private int id;
 }
